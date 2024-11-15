c54e5c1a1ee3d3232059d6c8ec88e62b5a303242
code-gio <138194389+code-gio@users.noreply.github.com>
2024-11-15 11:49:47 -0600

    deleted console logs

--- src/routes/docs/[slug]/+page.ts
+++ src/routes/docs/[slug]/+page.ts
@@ -2,9 +2,7 @@ import type { EntryGenerator, PageLoad } from './$types.js';
 import { getDoc } from '$lib/utils.js';
 
 export const load: PageLoad = async ({ params }) => {
-    console.log('params', params);
     const { component, title, metadata } = await getDoc(params.slug);
-    console.log('metadata', metadata);
     return {
         component,
         metadata,
@@ -13,7 +11,6 @@ export const load: PageLoad = async ({ params }) => {
 };
 
 export const entries: EntryGenerator = () => {
-    console.info('Prerendering /docs');
     const modules = import.meta.glob('/src/content/**/*.md');
     const entries = [];
 
